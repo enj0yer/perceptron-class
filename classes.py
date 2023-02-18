0270a734b0a74fc9c04211e29dc42dd2b545157e
Enj0yer <kulagin2002ilya@gmail.com>
2023-02-18 17:59:34 +0400

    - Removed first study algorythm  - Split create method to several methods

--- classes.py
+++ classes.py
@@ -11,11 +11,22 @@ class Perceptron:
         if inputs:
             self.__create(inputs, weights)
 
-    def __create(self, inputs: list[float], weights: list[list[list[float]]]):
-        self.__weights = [[[(random.random() / 2 - 0.25) for k in range(self.__amount_of_neurons[i - 1])] for j in range(self.__amount_of_neurons[i])] for i in range(self.__amount_of_layers) if i != 0] if not weights else weights
-        self.__states = [[0.0 for j in range(self.__amount_of_neurons[i])] for i in range(self.__amount_of_layers)]
-
+    def __create(self, inputs: list[float], weights: list[list[list[float]]] = None):
+        self.__create_states()
+        if weights:
+            self.__weights = weights
+        else:
+            self.__create_weights()
         self.__init_input(inputs)
+
+    def __create_states(self, multiplier: float = None, summand: float = None):
+        if not multiplier and not summand:
+            self.__states = [[0.0 for j in range(self.__amount_of_neurons[i])] for i in range(self.__amount_of_layers)]
+        else:
+            self.__states = [[(random.random() * multiplier + summand) for j in range(self.__amount_of_neurons[i])] for i in range(self.__amount_of_layers)]
+
+    def __create_weights(self, multiplier: float = 1.0, summand: float = 0.0):
+        self.__weights = [[[(random.random() * multiplier + summand) for k in range(self.__amount_of_neurons[i - 1])] for j in range(self.__amount_of_neurons[i])] for i in range(self.__amount_of_layers) if i != 0]
 
     def __init_input(self, inputs: list[float]):
         for i in range(len(inputs)):
@@ -56,39 +67,8 @@ class Perceptron:
 
                 self.__states[i][j] = self.__activate(state)
 
-    def __back_propagation(self):
-        pass
-
     def study(self, inputs: list[list[float]], outputs: list[list[float]], epoches: int = 1):
-
-        delta = [[0.0 for j in range(self.__amount_of_neurons[i] + 1)] for i in range(self.__amount_of_layers)]
-
-        for i in range(epoches):
-            rn = [j for j in range(len(inputs[0]))]
-
-            img_index = 0
-            for j in range(len(inputs)):
-                img_index = rn[i]
-                self.__init_input(inputs[j])
-                self.operate()
-                result = self.output
-
-                for r in range(self.__amount_of_neurons[self.__amount_of_layers - 1]):
-                    delta[self.__amount_of_layers - 1][r] = (self.__states[self.__amount_of_layers - 1][r] * (1 - self.__states[self.__amount_of_layers - 1][r])) * (outputs[img_index][r] - result[r])
-
-                for w in range(self.__amount_of_neurons[self.__amount_of_layers - 2] + 1):
-                    self.__weights[self.__amount_of_layers - 1][r][w] += self.__states[self.__amount_of_layers - 2][w] * delta[self.__amount_of_layers - 1][r]
-
-                for t in range(self.__amount_of_layers - 2, 0, -1):
-                    for r in range(1, self.__amount_of_neurons[t] + 1):
-                        _sum = 0
-                        for s in range(1, self.__amount_of_neurons[t + 1] + 1):
-                            _sum += delta[t + 1][s] * self.__weights[t + 1][s][r]
-
-                        delta[t][r] = _sum * self.__states[t][r] * (1 - self.__states[t][r])
-
-                        for a in range(self.__amount_of_neurons[t - 1] + 1):
-                            self.__weights[t][r][a] += self.__states[t - 1][a] * delta[t][r]
+        pass
 
     def __str__(self):
         return self.output
