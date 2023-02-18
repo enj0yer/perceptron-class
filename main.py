28c72205ec4639cf1be66af921f7e7952f7f55f8
Enj0yer <kulagin2002ilya@gmail.com>
2023-02-18 10:47:51 +0400

    - No functional changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 
 from classes import Perceptron
 
-amount_of_neuron = [5, 3, 2]
+amount_of_neuron = [5, 3, 10]
 inputs = [random.random(), random.random(), random.random(), random.random(), random.random()]
 perceptron = Perceptron(amount_of_neuron, inputs)
 
