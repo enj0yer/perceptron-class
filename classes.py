e4ff47272ba6e958cb2c32a453bcdfaecba22f37
Enj0yer <kulagin2002ilya@gmail.com>
2023-02-18 11:08:50 +0400

    - Some changes

--- classes.py
+++ classes.py
@@ -3,15 +3,16 @@ import random
 
 
 class Perceptron:
-    def __init__(self, amount_of_neurons: list[int], inputs: list[float]):
+    def __init__(self, amount_of_neurons: list[int], inputs: list[float] = None, weights: list[list[list[float]]] = None):
         self.__amount_of_layers = len(amount_of_neurons)
         self.__amount_of_neurons = amount_of_neurons
         self.__weights = None
         self.__states = None
-        self.__create(inputs)
+        if inputs:
+            self.__create(inputs, weights)
 
-    def __create(self, inputs: list[float]):
-        self.__weights = [[[random.random() for k in range(self.__amount_of_neurons[i - 1])] for j in range(self.__amount_of_neurons[i])] for i in range(self.__amount_of_layers) if i != 0]
+    def __create(self, inputs: list[float], weights: list[list[list[float]]]):
+        self.__weights = [[[random.random() for k in range(self.__amount_of_neurons[i - 1])] for j in range(self.__amount_of_neurons[i])] for i in range(self.__amount_of_layers) if i != 0] if not weights else weights
         self.__states = [[0.0 for j in range(self.__amount_of_neurons[i])] for i in range(self.__amount_of_layers)]
 
         for i in range(len(inputs)):
@@ -25,16 +26,15 @@ class Perceptron:
     def input(self) -> list[float]:
         return self.__states[0]
 
+    @input.setter
+    def input(self, _input: list[float]):
+        self.__states[0] = _input
+
     @property
     def output(self) -> list[float]:
         return self.__states[-1]
 
-    def operate(self, iterations: int = 1):
-        for _ in range(iterations):
-            self.__forward()
-            self.__back_propagation()
-
-    def __forward(self):
+    def operate(self):
         for i in range(1, self.__amount_of_layers):
             for j in range(self.__amount_of_neurons[i]):
                 state = 0
@@ -45,4 +45,7 @@ class Perceptron:
                 self.__states[i][j] = self.__activate(state)
 
     def __back_propagation(self):
+        pass
+
+    def study(self, inputs: list[list[float]], outputs: list[list[float]], epoches: int = 1):
         pass
