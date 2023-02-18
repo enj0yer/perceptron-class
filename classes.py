c5453fd15f8bb99e06da098c9165305e8a5944e8
Enj0yer <kulagin2002ilya@gmail.com>
2023-02-18 16:35:53 +0400

    - First attempt of study algorythm (not working)

--- classes.py
+++ classes.py
@@ -12,9 +12,12 @@ class Perceptron:
             self.__create(inputs, weights)
 
     def __create(self, inputs: list[float], weights: list[list[list[float]]]):
-        self.__weights = [[[random.random() for k in range(self.__amount_of_neurons[i - 1])] for j in range(self.__amount_of_neurons[i])] for i in range(self.__amount_of_layers) if i != 0] if not weights else weights
+        self.__weights = [[[(random.random() / 2 - 0.25) for k in range(self.__amount_of_neurons[i - 1])] for j in range(self.__amount_of_neurons[i])] for i in range(self.__amount_of_layers) if i != 0] if not weights else weights
         self.__states = [[0.0 for j in range(self.__amount_of_neurons[i])] for i in range(self.__amount_of_layers)]
 
+        self.__init_input(inputs)
+
+    def __init_input(self, inputs: list[float]):
         for i in range(len(inputs)):
             self.__states[0][i] = inputs[i]
 
@@ -34,12 +37,21 @@ class Perceptron:
     def output(self) -> list[float]:
         return self.__states[-1]
 
-    def operate(self):
+    @property
+    def weights(self) -> list[list[list[float]]]:
+        return self.__weights
+
+    @weights.setter
+    def weights(self, _weights):
+        self.__weights = _weights
+
+    def operate(self, _input: list[float] = None):
+        if _input:
+            self.__init_input(_input)
         for i in range(1, self.__amount_of_layers):
             for j in range(self.__amount_of_neurons[i]):
                 state = 0
                 for k in range(self.__amount_of_neurons[i - 1]):
-                    print(i, j, k)
                     state += self.__states[i - 1][k] * self.__weights[i - 1][j][k]
 
                 self.__states[i][j] = self.__activate(state)
@@ -48,4 +60,35 @@ class Perceptron:
         pass
 
     def study(self, inputs: list[list[float]], outputs: list[list[float]], epoches: int = 1):
-        pass
+
+        delta = [[0.0 for j in range(self.__amount_of_neurons[i] + 1)] for i in range(self.__amount_of_layers)]
+
+        for i in range(epoches):
+            rn = [j for j in range(len(inputs[0]))]
+
+            img_index = 0
+            for j in range(len(inputs)):
+                img_index = rn[i]
+                self.__init_input(inputs[j])
+                self.operate()
+                result = self.output
+
+                for r in range(self.__amount_of_neurons[self.__amount_of_layers - 1]):
+                    delta[self.__amount_of_layers - 1][r] = (self.__states[self.__amount_of_layers - 1][r] * (1 - self.__states[self.__amount_of_layers - 1][r])) * (outputs[img_index][r] - result[r])
+
+                for w in range(self.__amount_of_neurons[self.__amount_of_layers - 2] + 1):
+                    self.__weights[self.__amount_of_layers - 1][r][w] += self.__states[self.__amount_of_layers - 2][w] * delta[self.__amount_of_layers - 1][r]
+
+                for t in range(self.__amount_of_layers - 2, 0, -1):
+                    for r in range(1, self.__amount_of_neurons[t] + 1):
+                        _sum = 0
+                        for s in range(1, self.__amount_of_neurons[t + 1] + 1):
+                            _sum += delta[t + 1][s] * self.__weights[t + 1][s][r]
+
+                        delta[t][r] = _sum * self.__states[t][r] * (1 - self.__states[t][r])
+
+                        for a in range(self.__amount_of_neurons[t - 1] + 1):
+                            self.__weights[t][r][a] += self.__states[t - 1][a] * delta[t][r]
+
+    def __str__(self):
+        return self.output
--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import random
 
 from classes import Perceptron
 
-amount_of_neuron = [5, 3, 10]
-inputs = [random.random(), random.random(), random.random(), random.random(), random.random()]
-perceptron = Perceptron(amount_of_neuron, inputs)
+input_amount = 400
+output_amount = 10
 
-perceptron.operate()
+amount_of_neuron = [input_amount, 25, output_amount]
+perceptron = Perceptron(amount_of_neuron)
 
-print(perceptron.output)
+inputs = [[(i + j) for j in range(input_amount)] for i in range(20)]
+outputs = [[(i * j) for j in range(output_amount)] for i in range(20)]
+
+perceptron.study(inputs, outputs, 20)
+perceptron.operate([(i + i) for i in range(input_amount)])
+
+print(perceptron)
